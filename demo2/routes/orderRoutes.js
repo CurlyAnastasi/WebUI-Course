a6ac106c895298ce5e59482b30fb9de482cc9e5a
Anastasiya Penya <nastyapenya@gmail.com>
2021-05-05 01:57:54 +0300

    demo2

--- demo2/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const orderRoutes = express.Router();
-const orderController = require('../controllers/orderController');
-
-
-orderRoutes.post('/', orderController.postOrder);
-
-
-module.exports = orderRoutes;
